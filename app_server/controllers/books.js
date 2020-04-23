7f616773f7a41de5796f7eb71c1d922250e68563
Cory Gilliam <imaaxa@gmail.com>
2020-04-23 11:26:22 -0400

    Chapter 4 changes ( some are temp)

---
+++ app_server/controllers/books.js
@@ -0,0 +1,11 @@
+// GET About page
+const index = (req, res) => {
+  res.render('books', {
+    title: 'Books',
+    body: '<p>Sit labore ut irure adipisicing est sint incididunt voluptate irure velit aliquip duis. Tempor quis ad veniam do nostrud id irure ipsum. Ullamco nisi tempor exercitation do pariatur amet labore. Quis voluptate elit esse officia consectetur magna culpa adipisicing in pariatur adipisicing anim. Id dolore ad ea veniam. Amet ipsum tempor adipisicing in commodo Lorem commodo do minim sint mollit nostrud.</p><p>Do sunt mollit in ipsum excepteur adipisicing minim magna deserunt ullamco laboris anim. Sint anim voluptate fugiat laboris labore quis mollit nostrud laborum adipisicing nisi. Occaecat laboris tempor amet exercitation aliquip voluptate cupidatat do. Laborum magna incididunt est consequat cupidatat ea aliqua. Quis nisi tempor qui veniam do velit amet ex. Anim minim consectetur et elit cupidatat nulla quis consequat dolor duis duis cupidatat aliquip qui. Nulla ut laboris incididunt aliqua commodo ea quis aliqua aliquip magna cupidatat sunt ea laborum.</p><p>Enim id reprehenderit dolore esse sit excepteur non aliqua est. Voluptate eiusmod ut sunt eu. Mollit sint aute eu qui deserunt ipsum culpa eu anim eu.</p>',
+    rating: 4
+  });
+};
+
+// Export
+module.exports = { index };
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -4,10 +4,12 @@ const router = express.Router();
 // Controlers
 const home = require('../controllers/home'); // Home controler
 const about = require('../controllers/about'); // About controler
+const books = require('../controllers/books'); // Books controler
 
 // Routes
 router.get('/', home.index); // Home page
 router.get('/about', about.index); // About page
+router.get('/books', books.index); // Books page
 
 // Export router
 module.exports = router;
