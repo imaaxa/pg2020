0a611b2fd141a7522a4bcd799630b1604203c44c
Cory Gilliam <imaaxa@gmail.com>
2020-04-25 08:08:37 -0400

    Modified comments

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const path = require('path');
 
+// Require Database connection data
 require('./app_api/models/db');
 
+// Set the Route paths
 const indexRouter = require('./app_server/routes/index');
 const apiRouter = require('./app_api/routes/index');
 
@@ -16,27 +18,29 @@ const app = express();
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'pug');
 
+// Set up middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Set routes
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  // Set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // Render the error page
   res.status(err.status || 500);
   res.render('error');
 });
